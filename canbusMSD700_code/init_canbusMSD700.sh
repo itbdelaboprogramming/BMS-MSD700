d39c8f8cc56b7f7db5b026afad9f6de9758b647a
iwanicho <nichol999homework@gmail.com>
2023-04-05 11:20:30 +0900

    init script added

--- canbusMSD700_code/init_canbusMSD700.sh
+++ canbusMSD700_code/init_canbusMSD700.sh
@@ -15,7 +15,7 @@ sudo raspi-config nonint do_spi 0
 sleep 1
 
 # Enable execute (run program) privilege for the python script
-sudo chmod +x canbusMSD700.py
+sudo chmod +x /home/$(logname)/canbusMSD700_code/canbusMSD700.py
 sleep 1
 # Install pip for python library manager
 sudo apt install python3-pip
@@ -25,9 +25,9 @@ sudo pip3 install python-can
 sudo pip3 install pymysql
 sleep 1
 # Check whether the command line is already exist in /etc/rc.local
-if ! sudo grep -q "sudo sh /home/$(logname)/canbusMSD700.py &" /etc/rc.local; then
+if ! sudo grep -q "sudo sh /home/$(logname)/canbusMSD700_code/canbusMSD700.py &" /etc/rc.local; then
     # Append the file into /etc/rc.local (1 line above "exit 0") to enable automatic run after reboot 
-    sudo sed -i "$(($(wc -l < /etc/rc.local)-0))i sudo sh /home/$(logname)/canbusMSD700.py &" /etc/rc.local
+    sudo sed -i "$(($(wc -l < /etc/rc.local)-0))i sudo sh /home/$(logname)/canbusMSD700_code/canbusMSD700.py &" /etc/rc.local
 fi
 sleep 1
 # Enable execute (run program) privilege /etc/rc.local
